1c535a476402d698a704d79f9ca3750f503e9048
Usman S. <ui.sabuwala@gmail.com>
2025-08-11 10:00:58 +0530

    add auth schema change migration files

---
+++ src/lib/db/migrations/0001_organic_prodigy.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "users" ADD COLUMN "phone" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "city" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "country" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "additional_info" text;
